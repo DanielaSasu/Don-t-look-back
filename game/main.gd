4b973ea16d2a9c096fd28f353280c1eb92bee6df
Danii <sasu.daniela13@gmail.com>
2026-06-03 19:51:54 +0300

    Fixed the timer

--- game/main.gd
+++ game/main.gd
@@ -3,7 +3,11 @@ extends Node2D
 var jump_timer: Timer
 var current_context: String = ""
 
+@onready var timer_bar = %TimerBar
+
 func _ready() -> void:
+	timer_bar.hide()
+	
 	jump_timer = Timer.new()
 	jump_timer.one_shot = true
 	jump_timer.timeout.connect(_on_timer_timeout)
@@ -13,18 +17,31 @@ func _ready() -> void:
 	
 	Dialogic.start("the_jump")
 
+func _process(delta: float) -> void:
+	if not jump_timer.is_stopped():
+		timer_bar.value = jump_timer.time_left
+
 func _on_dialogic_signal(argument: String) -> void:
 	
 	if argument == "stop_timer":
 		jump_timer.stop()
 		current_context = ""
+		timer_bar.hide()
 		return
 		
 	if argument.begins_with("start_timer_"):
 		current_context = argument.replace("start_timer_", "")
-		jump_timer.start(5.0)
+		
+		jump_timer.wait_time = 5.0
+		jump_timer.start()
+		
+		timer_bar.max_value = 5.0
+		timer_bar.value = 5.0
+		timer_bar.show()
 
 func _on_timer_timeout() -> void:
+	timer_bar.hide()
+	
 	var random_label: String = ""
 	
 	match current_context:
@@ -48,3 +65,5 @@ func _on_timer_timeout() -> void:
 			Dialogic.Choices.hide_all_choices()
 		
 		Dialogic.Jump.jump_to_label(random_label)
+		
+		Dialogic.handle_next_event()
